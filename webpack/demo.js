811db0150f0f0147ee05c9fd7fb370edfd27a37d
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-03-29 19:02:02 -0700

    Exclude deps from bundle, add esm output and sourcemaps

--- webpack/demo.js
+++ webpack/demo.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
-const merge = require("webpack-merge");
+const merge = require("webpack-merge").default;
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -32,11 +32,7 @@ module.exports = merge(baseConfig, {
   },
   devServer: {
     hot: true,
-    hotOnly: true,
-    contentBase: path.resolve(__dirname, "../demo/dist"),
-    publicPath: "/",
     historyApiFallback: true,
-    stats: "minimal",
     port: 8080
   },
   plugins: [
